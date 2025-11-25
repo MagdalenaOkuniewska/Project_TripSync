b69097f129f290cc98418feadc83671f32476881
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-11-25 15:42:54 +0100

    feat:Added TripForm

--- src/trips/views.py
+++ src/trips/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Trip
+
+## TODO logika przy zapisie, że ustawia zalogowane użytkowniak jako owern'a.
 
