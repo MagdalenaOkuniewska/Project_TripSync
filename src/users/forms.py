d1b1a2f9ca2c90057c79566c5ccde8aa2e8f46e8
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-02 15:49:30 +0100

    feat:Added register view

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class UserRegistrationForm(UserCreationForm):
@@ -9,7 +9,12 @@ class UserRegistrationForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
     class Meta:
         model = CustomUser
         fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'avatar']
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+
+
+
+
