0e0be0f0ab40b09b58c9de0609fb741c75d3e819
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-02 15:31:08 +0100

    feat:Added registration form to forms

---
+++ src/users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email']
