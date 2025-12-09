97b96e126b9e9331ec550f203a6d926e52c2cb73
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-09 16:25:45 +0100

    feat:Implemented password reset functionality: - Added 4 password reset templates (reset, done, confirm, complete) - Added Forgot password link in login page - Configured console email backend in settings
    Note: Windows users may need to patch django/core/mail/utils.py
    to fix UnicodeError in get_fqdn() method

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationView, ProfileView
+from .views import RegistrationView, ProfileView, CustomPasswordResetView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -7,5 +7,10 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', next_page = '/users/profile/' ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
