d45bcf4aa7efe313f331944774ec719bdcf929e0
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-09 15:06:39 +0100

    feat:Added CustomResetPasswordView to users/views

--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.views import PasswordResetView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import UserRegistrationForm, UserUpdateForm
+from .forms import UserRegistrationForm, UserUpdateForm, CustomPasswordResetForm
 from .models import CustomUser
 
 class RegistrationView(CreateView):
@@ -30,5 +31,17 @@ class ProfileView(LoginRequiredMixin, UpdateView):
         messages.success(self.request, f'Your account has been updated!')
         return super().form_valid(form)
 
+
+class CustomPasswordResetView(PasswordResetView):
+    form_class = CustomPasswordResetForm
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    subject_template_name = 'users/password_reset_subject.txt'
+    success_url = reverse_lazy('password_reset_done')
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        messages.success(self.request, f'Password reset email was sent to "{email}". Please check your inbox and follow instructions.')
+        return super().form_valid(form)
 
 
