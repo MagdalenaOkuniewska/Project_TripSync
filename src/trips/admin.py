743734846f7d1a139040b170a05a0a7d8e935b74
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-10 14:50:36 +0100

    feat: Registered TripMember model in admin

--- src/trips/admin.py
+++ src/trips/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, TripMember
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
     list_display = ('id',)
+
+@admin.register(TripMember)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('id', 'trip', 'user', 'role', 'status', 'joined_at')
