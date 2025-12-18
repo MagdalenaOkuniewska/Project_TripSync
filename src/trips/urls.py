a61046ee990ee5f59aee3a784bd8374a5c7bc51d
MagdalenaOkuniewska <magdaok3@gmail.com>
2025-12-18 17:56:01 +0100

    feat:Added TripMemberListView, HTML  template and URL path

--- src/trips/urls.py
+++ src/trips/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import TripListView,TripCreateView, TripDetailView, TripUpdateView, TripDeleteView, TripInviteCreateView, TripInviteListView, TripInviteCancelView, TripInviteSentListView,TripInviteRespondView
+from .views import TripListView,TripCreateView, TripDetailView, TripUpdateView, TripDeleteView, TripInviteCreateView, TripMemberListView,TripInviteListView, TripInviteCancelView, TripInviteSentListView,TripInviteRespondView
 
 urlpatterns = [
     path('', TripListView.as_view(), name='trip-list'),
@@ -9,6 +9,8 @@ urlpatterns = [
     path('<int:pk>/delete/', TripDeleteView.as_view(), name='trip-delete'),
 
     path('<int:trip_id>/invite/', TripInviteCreateView.as_view(), name='trip-invite-create'),
+    path('<int:trip_id>/members/', TripMemberListView.as_view(), name='trip-member-list'),
+
     path('invites/', TripInviteListView.as_view(), name='trip-invite-list'),
     path('invites/sent/', TripInviteSentListView.as_view(), name='trip-invite-sent'),
     path('invites/<int:pk>/cancel/', TripInviteCancelView.as_view(), name='trip-invite-cancel'),
--- src/trips/views.py
+++ src/trips/views.py
@@ -99,7 +99,7 @@ class TripInviteCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     fields = ['user']
 
     def test_func(self):
-        self.trip = Trip.objects.get(id=self.kwargs['trip_id'])
+        self.trip = Trip.objects.get(id=self.kwargs['trip_id']) #self -> atrybut klasy użyty w innych metodach poniżej
         return self.trip.owner == self.request.user
 
     def handle_no_permission(self):
@@ -212,4 +212,25 @@ class TripInviteCancelView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     def get_success_url(self):
         return reverse_lazy('trip-detail', kwargs={'pk': self.object.trip.id})
 
+class TripMemberListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = TripMember
+    template_name = 'trips/trip_member_list.html'
+    context_object_name = 'members'
+
+    def test_func(self):
+        self.trip = Trip.objects.get(pk=self.kwargs['trip_id'])
+        return self.trip.is_owner(self.request.user) or self.trip.is_participant(self.request.user)
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'You cannot view members of this Trip - only members and owners have access.')
+        return redirect('trip-list')
+
+    def get_queryset(self):
+        self.trip = Trip.objects.get(pk=self.kwargs['trip_id'])
+        return self.trip.members.all().order_by('-role', '-joined_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['trip'] = self.trip  #dodaje Trip do templ
+        return context
 
