b03773740765ba69bb2d1e3bce083e254ee9f0f2
MagdalenaOkuniewska <magdaok3@gmail.com>
2026-01-27 15:52:18 +0100

    Added Note model

--- src/notes/models.py
+++ src/notes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from trips.models import Trip
 
-# Create your models here.
+User = get_user_model()
+
+
+class Note(models.Model):
+    NOTE_TYPE_CHOICES = [("private", "Private"), ("public", "Public")]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="notes")
+
+    title = models.CharField(max_length=200)
+    note_type = models.CharField(
+        max_length=10, choices=NOTE_TYPE_CHOICES, default="private"
+    )
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def is_owner(self, user):
+        return self.owner == user
+
+    def can_edit(self, user):
+        return self.is_owner(user)
+
+    def can_delete(self, user):
+        return self.can_edit(user)
+
+    def can_view(self, user):
+        if self.note_type == "private":
+            return self.is_owner(user)
+        else:
+            return self.trip.is_owner(user) or self.trip.is_participant(user)
+
+    def __str__(self):
+        return f" Note: {self.title} - Trip: {self.trip.title}"
