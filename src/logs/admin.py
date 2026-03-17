4726fdaa43e25bb607cb58753f64f780377af655
MagdalenaOkuniewska <magdaok3@gmail.com>
2026-03-17 14:52:19 +0100

    Registered logs in admin

--- src/logs/admin.py
+++ src/logs/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import AuditLog
 
-# Register your models here.
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "action",
+        "content_type",
+        "object_id",
+        "performed_by",
+        "affected_user",
+        "timestamp",
+    )
+    list_filter = ("action",)
+    readonly_fields = (
+        "content_type",
+        "object_id",
+        "content_object",
+        "action",
+        "performed_by",
+        "affected_user",
+        "timestamp",
+        "extra_data",
+    )
